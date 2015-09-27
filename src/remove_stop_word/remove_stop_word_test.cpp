62db0fbd890827cb4ff3bfcd691ed2686bf0ad8c
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-09-27 13:49:22 -0600

    Add "RemoveStopWords" function
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/remove_stop_word/remove_stop_word_test.cpp
+++ src/remove_stop_word/remove_stop_word_test.cpp
@@ -29,12 +29,25 @@ namespace preprocessing {
     Split(s, delim, elems);
     return elems;
   }
+  void RemoveWord(const std::string &word_to_remove, const std::string &input,
+      std::string &output) {
+      using namespace std::regex_constants;
+    std::ostringstream oss; 
+    oss << "(^" << word_to_remove << "\\b\\s*)";
+    oss << "|(\\s*\\b" << word_to_remove << "\\b)";
+    oss << "|(\\s*\\b" << word_to_remove << "\\b\\s*.$)";
+    std::regex words(oss.str(), icase);
+    output = std::regex_replace(input, words, ""); 
 
-  void RemoveStopWords(const std::string &input, std::string &output) 
-  {
   }
 
-
+  void RemoveStopWords(const std::vector<std::string> &stop_words, const std::string &input, std::string &output) 
+  {
+    output = input; 
+    std::for_each(stop_words.begin(), stop_words.end(), [&](std::string stop_word) {
+      RemoveWord(stop_word, output, output); 
+    });
+  }
 
 }
 
@@ -54,4 +67,26 @@ TEST(PreProcessing, MakeCommaVector) {
   ASSERT_EQ("stop", output[i++]); 
   ASSERT_EQ("word", output[i++]); 
   ASSERT_EQ("cool", output[i++]); 
+}
+
+TEST(PreProcessing, RemoveWord) {
+  const std::string word_to_remove{"this"}; 
+  std::string test{"this Don't do ThiS to me this."}; 
+  std::string output; 
+  pp::RemoveWord(word_to_remove, test, output);
+  ASSERT_EQ("Don't do to me.", output); 
+}
+
+
+
+TEST(PreProcessing, RemoveStopWords) {
+  const std::vector<std::string> stop_words{"is", "sentence"}; 
+  const std::string test_sentence{"This is my sentence"}; 
+  const std::string expr{"(\\s*This\\s*my\\s*)"}; 
+  std::regex truth(expr);
+  std::string output; 
+  pp::RemoveStopWords(stop_words, test_sentence, output); 
+  bool match = std::regex_match(output.cbegin(), output.cend(), truth); 
+  ASSERT_TRUE(match) << "Output is: '" << output 
+  << "'. it should match this regex: " << expr << std::endl;; 
 }
