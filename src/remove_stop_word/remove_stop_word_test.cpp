0ed7c5f37fbfa485a25de208f299fd295b929868
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-09-27 09:37:24 -0600

    Get stop words via csv
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/remove_stop_word/remove_stop_word_test.cpp
+++ src/remove_stop_word/remove_stop_word_test.cpp
@@ -1,8 +1,9 @@
-
-
 #include <iostream>
 #include <string> 
 #include <regex>
+#include <vector> 
+#include <iterator> 
+#include <sstream>
 
 #include <gtest/gtest.h>
 
@@ -13,6 +14,28 @@ namespace preprocessing {
     output = std::regex_replace(input, words, ""); 
 
   }
+
+  std::vector<std::string> &Split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+      elems.push_back(item);
+    }
+    return elems;
+  }
+
+  std::vector<std::string> Split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    Split(s, delim, elems);
+    return elems;
+  }
+
+  void RemoveStopWords(const std::string &input, std::string &output) 
+  {
+  }
+
+
+
 }
 
 namespace pp = ::preprocessing; 
@@ -21,4 +44,14 @@ TEST(PreProcessing, RemovePunctuation) {
   const std::string truth{"a b"}; 
   pp::RemovePunctuation(input, input);  
   ASSERT_STREQ(truth.c_str(), input.c_str()); 
+}
+
+TEST(PreProcessing, MakeCommaVector) {
+  const std::string input{"stop,word,cool"}; 
+  const std::vector<std::string> output = pp::Split(input, ',');
+  size_t i = 0; 
+  ASSERT_EQ(3, output.size());
+  ASSERT_EQ("stop", output[i++]); 
+  ASSERT_EQ("word", output[i++]); 
+  ASSERT_EQ("cool", output[i++]); 
 }
