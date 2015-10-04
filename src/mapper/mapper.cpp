d7d2320a50ad86ea37b742ebe3de49fc930259e6
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-10-04 15:25:05 -0600

    Move stop words to preprocessing namespace
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/mapper/mapper.cpp
+++ src/mapper/mapper.cpp
@@ -1,9 +1,6 @@
 // Usage example: 
+// mapper < foo.txt > output.txt
 // 
-// ./preprocess_file ../files/stop_words.txt < .../files/input.txt > ../files/output.txt
-//
-// The above program loads stops words from a file and removes the stop
-// words for the "input.txt" file and outputs it to the "output.txt" file. 
 #include <vector> 
 #include <string> 
 #include <iostream> 
@@ -13,16 +10,13 @@
 
 #include "preprocessing.h"
 
-const std::vector<std::string> stop_words{
-  "a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"};
 
-void Mapper(const std::vector<std::string>
-    stop_words) {
+void Mapper() {
   std::string line;
   while (std::cin) {
     while ( getline (std::cin, line) )
     {
-      preprocessing::PreprocessLine(line, stop_words); 
+      preprocessing::PreprocessLine(line, preprocessing::stop_words); 
     }
   }
 }
@@ -30,6 +24,6 @@ void Mapper(const std::vector<std::string>
 
 int main (int argc, char *argv[]) {
 
-  Mapper(stop_words);
+  Mapper();
 
 }
--- src/preprocessing/preprocessing.h
+++ src/preprocessing/preprocessing.h
@@ -1,4 +1,5 @@
 #ifndef _SENTIMENT_ANALYSIS_PREPROCESSING_H_
+
 #define _SENTIMENT_ANALYSIS_PREPROCESSING_H_
 
 #include <iostream>
@@ -11,6 +12,10 @@
 #include <stemming/english_stem.h>
 
 namespace preprocessing {
+
+const std::vector<std::string> stop_words{
+  "a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"};
+
   void RemovePunctuation(const std::string &input, std::string &output) {
     std::regex words("[^A-Za-z\\s]");
     output = std::regex_replace(input, words, ""); 
